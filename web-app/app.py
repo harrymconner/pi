a9195708e63dc84266a33dce50ff097766894de8
Harry Conner <harrymconner@gmail.com>
2024-12-29 22:27:59 -0500

    try to interact with db

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,22 @@
+import os
+
+import bw
+import dotenv
 import fastapi
+import psycopg
+
+dotenv.load_dotenv()
 
 app = fastapi.FastAPI()
 
 
 @app.get("/")
-async def root() -> dict[str, str]:
-    return {"response": "hello world"}
+async def root() -> tuple[str] | None:
+    user = bw.get_secret(os.environ["POSTGRES_USER"])
+    password = bw.get_secret(os.environ["POSTGRES_PASSWORD"])
+    connection_string = f"host=postgres user={user} password={password}"
+
+    with psycopg.connect(connection_string) as conn:
+        cur = conn.cursor()
+
+        return cur.execute("SELECT 'hello world'").fetchone()
