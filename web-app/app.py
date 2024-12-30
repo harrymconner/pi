a7451db92715a17aee0d30e90ac9fe12817d6256
Harry Conner <harrymconner@gmail.com>
2024-12-30 11:24:41 -0500

    use env file per service

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,5 @@
 import os
 
-import bw
 import dotenv
 import fastapi
 import psycopg
@@ -12,8 +11,8 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 async def root() -> tuple[str] | None:
-    user = bw.get_secret(os.environ["POSTGRES_USER"])
-    password = bw.get_secret(os.environ["POSTGRES_PASSWORD"])
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
     connection_string = f"host=postgres user={user} password={password}"
 
     with psycopg.connect(connection_string) as conn:
