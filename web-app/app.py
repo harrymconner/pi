8a49871ba5d1d33fb7ef9823d75acaf0038fd07f
Harry Conner <harrymconner@gmail.com>
2024-12-31 09:13:50 -0500

    first fully working version

--- web-app/app.py
+++ web-app/app.py
@@ -10,12 +10,12 @@ app = fastapi.FastAPI()
 
 
 @app.get("/")
-async def root() -> tuple[str] | None:
+async def root() -> list[tuple[int, float]]:
     user = os.environ["POSTGRES_USER"]
     password = os.environ["POSTGRES_PASSWORD"]
-    connection_string = f"host=postgres user={user} password={password}"
+    connection_string = f"postgresql://{user}:{password}@postgres:5432/dwh"
 
     with psycopg.connect(connection_string) as conn:
         cur = conn.cursor()
 
-        return cur.execute("SELECT 'hello world'").fetchone()
+        return cur.execute("SELECT * FROM prod.transactions").fetchall()
